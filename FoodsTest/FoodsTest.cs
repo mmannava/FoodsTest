30f271845bf9264c09183c14caf254397ad36690
mmannava <manasapriyamvadamannava@yahoo.com>
2019-01-06 16:01:50 -0600

    Add project files.

---
+++ FoodsTest/FoodsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Foods;
+using static Foods.Program;
+
+namespace FoodsTest
+{
+    [TestFixture]
+    public class FoodsTests
+    {
+        List<Food> foods;
+
+        [Test]
+        public void Checkdetails()
+        {
+            Program p = new Program();
+           
+        }
+
+    }
+}
